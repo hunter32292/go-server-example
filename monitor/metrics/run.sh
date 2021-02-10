fb6358430a0605e6f2e3911bb29c7635c096e2dc
John Stupka <jrstupkadev@gmail.com>
2021-02-09 19:04:03 -0800

    setup run.sh with trace interface

--- monitor/metrics/run.sh
+++ monitor/metrics/run.sh
@@ -1,6 +1,8 @@
-echo "Running docker version of promethues sharing host network..."
+echo "Running docker version of prometheus sharing host network..."
 
 docker run \
     --network host \
     -v $(pwd):/etc/prometheus \
     prom/prometheus
+
+echo "To access prometheus goto http://localhost:8080"
